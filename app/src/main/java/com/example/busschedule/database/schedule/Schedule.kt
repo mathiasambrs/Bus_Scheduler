a362210c9b7f93eb658ed142b913b310d2333b1c
mathiasambrs <mathias.ambrs@eduge.ch>
2023-02-27 15:52:46 +0100

    16h

--- app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
+++ app/src/main/java/com/example/busschedule/database/schedule/Schedule.kt
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+//Mathias AMBROSI
 @Entity
 data class Schedule(
     @PrimaryKey val id: Int,
