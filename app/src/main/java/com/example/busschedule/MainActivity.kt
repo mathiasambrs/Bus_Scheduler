08245765adac591cbcf7bac780f620d45e8e6f02
mathiasambrs <mathias.ambrs@eduge.ch>
2023-03-06 13:02:21 +0100

    partie 9 + Ajout des noms

--- app/src/main/java/com/example/busschedule/MainActivity.kt
+++ app/src/main/java/com/example/busschedule/MainActivity.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 package com.example.busschedule
-
+//Mathias AMBROSI
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
